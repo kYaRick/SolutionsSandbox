94827576bcf481bbb967c73f7f698868c4e5f2d7
kYaRick <kilyshuk@gmail.com>
2023-02-17 15:42:26 +0200

    Example for simple Xunit and "FluentAssertions" was added.

---
+++ kYaSandbox.Tests/PingerTest.cs
@@ -0,0 +1,28 @@
+using Xunit;
+using kYaSandbox.PingerSpace;
+using FluentAssertions;
+
+namespace kYaSandbox.Tests
+{
+    public class PingerTest
+    {
+        [Fact]
+        public void Pinger_IsPing_True()
+        {
+            //Arrange.
+            //Action.
+            var result = Pinger.IsPing("test");
+            //Assert.
+            result.Should().BeTrue();
+        }
+        [Fact]
+        public void Pinger_IsPing_False()
+        {
+            //Arrange.
+            //Action.
+            var result = Pinger.IsPing("");
+            //Assert.
+            result.Should().BeFalse();
+        }
+    }
+}
---
+++ kYaSandbox/Pinger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kYaSandbox.PingerSpace;
+
+public static class Pinger
+{
+    public static bool IsPing(string url) => !String.IsNullOrEmpty(url);
+}
